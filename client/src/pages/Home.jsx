55f44246edadd2e135a7b47d81d4abf3adebb5f1
Ezra-11 <ezramngondo11@gmail.com>
2025-07-07 14:41:36 +0200

    updated the file name

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Hero from '../components/Hero'
-import FeatureSection from '../components/featureSection'
+import FeatureSection from '../components/FeatureSection'
 import Banners from '../components/Banners'
 import Testimonial from '../components/Testimonial'
 import Newsletter from '../components/Newsletter'
